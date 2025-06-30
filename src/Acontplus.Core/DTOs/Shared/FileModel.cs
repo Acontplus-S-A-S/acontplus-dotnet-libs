a9a2f86248b4d13bf3d91172545d2eceb777c6c8
Ivan Fernando Paz <ifer777@outlook.com>
2025-06-30 16:20:27 -0500

    Update version to 1.0.6 and modify FileModel
    - Bump project version in `Acontplus.Core.csproj` from 1.0.5 to 1.0.6.
    - Change `Content` property in `FileModel.cs` to be nullable, making it optional.

--- src/Acontplus.Core/DTOs/Shared/FileModel.cs
+++ src/Acontplus.Core/DTOs/Shared/FileModel.cs
@@ -4,6 +4,6 @@ public class FileModel
 {
     public string? FileName { get; set; }
     public string? ContentType { get; set; }
-    public required byte[] Content { get; set; }
+    public byte[]? Content { get; set; }
     public string? Base64 { get; set; }
 }
