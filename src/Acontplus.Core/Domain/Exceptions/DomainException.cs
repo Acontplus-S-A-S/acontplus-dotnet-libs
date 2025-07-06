cea6d6c0dd1e809f7ef4fc14a071a4dfe01118f5
Ivan Fernando Paz <ifer777@outlook.com>
2025-07-06 16:40:02 -0500

    Bump `Acontplus.Core` to `1.1.9` and refactor `DomainException` constructor for improved initialization consistency

--- src/Acontplus.Core/Domain/Exceptions/DomainException.cs
+++ src/Acontplus.Core/Domain/Exceptions/DomainException.cs
@@ -1,8 +1,13 @@
 ﻿namespace Acontplus.Core.Domain.Exceptions;
 
-public abstract class DomainException(ErrorType type, string code, string message, Exception? inner = null)
-    : Exception(message, inner)
+public abstract class DomainException : Exception
 {
-    public ErrorType ErrorType { get; } = type;
-    public string ErrorCode { get; } = code;
+    public ErrorType ErrorType { get; }
+    public string ErrorCode { get; }
+    public DomainException(ErrorType type, string code, string message, Exception? inner = null)
+        : base(message, inner)
+    {
+        ErrorType = type;
+        ErrorCode = code;
+    }
 }
