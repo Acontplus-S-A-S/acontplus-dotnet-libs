77929493186fecdabf87c7b01dbdb7750b42adad
Ivan Fernando Paz <ifer777@outlook.com>
2025-07-10 11:48:08 -0500

    Update Acontplus.Persistence.SqlServer to version 1.0.15 and introduce SQL string sanitization utility
    - Bumped version in Acontplus.Persistence.SqlServer.csproj to 1.0.15.
    - Added `SqlStringParam` class in `Validation` namespace for sanitizing SQL strings and preventing injection attacks.
    - Implemented regex-based sanitization logic to replace potentially harmful SQL patterns.

---
+++ src/Acontplus.Persistence.SqlServer/Validation/SqlStringParam.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Acontplus.Persistence.SqlServer.Validation;
+
+public static class SqlStringParam
+{
+    public static string Sanitize(string input)
+    {
+        var expression =
+            new Regex(@";|=|<|>| or | and |select 
+              | insert | update | drop | xp_ | --| exec"
+            );
+
+        var result =
+            expression.Replace(input, MatchEvaluatorHandler);
+
+        return result;
+    }
+
+    private static string MatchEvaluatorHandler(Match match)
+    {
+        //Replace the matched items with a blank string of 
+        //equal length
+        return new string(' ', match.Length);
+    }
+}
